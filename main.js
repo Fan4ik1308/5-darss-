1b680dbeeabaf4470ce3e891a3bdc3c7279571c8
Fan4ik1308 <moonaraves@gmail.com>
2025-02-15 10:15:05 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+// const Username = prompt("ism kriting")
+// console.log(Username.toUpperCase(   ));
+// alert(`${Username} ismda ${Username.length} harifdan iborat`)
+
+// console.log(importScripts,charAt(5));
+// console.log(importScripts,charCodeAt(6));
+
+// const oquvchi = {
+//     ism:"Elbek",
+//     yosh:13,
+//     coin:860,
+//     group:2094,
+//     uylangan:true,
+//     bolasi:false,
+// }
+
+// console.log(oquvchi);
+// const UserName = prompt("Ismingizni kiriting");
+// const UserSurname = prompt("Familya kiriting");
+// const UserAge = +prompt("Yosh kiriting");
+// const UserCoin = +prompt("Coin kiriting");
+// const UserLogin = prompt("Login kiriting");
+// const UserPassword = +prompt("Parol kiriting");
+// const UserUstoz = prompt("Ustozini kiriting");
+// const UserHp = prompt("model notebook kiriting");
+// const UserGroup = +prompt("Guruhingizni kiriting");
+
+
+// const oquvchi = {
+//     ism: UserName,
+//     Familya: UserSurname,
+//     yosh:UserAge,
+//     coin:UserCoin,
+//     Login:UserLogin,
+//     password:UserPassword,
+//     Ustoz:UserUstoz,
+//     Hp:UserHp,
+//     Group:UserGroup,
+// }
+// console.log(oquvchi)
+
+const Name = prompt("Ismingizni kiriting");
+const Id = prompt("spaceid kiriting");
+const Password = prompt("Password kiriting");
+
+
